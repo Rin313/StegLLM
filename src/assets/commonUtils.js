0d83feb0658f906f3c4da9697ea830fa5ab6ed8b
Rin313 <61936230+Rin313@users.noreply.github.com>
2025-09-01 02:14:47 +0800

    refactor: utils

--- src/assets/commonUtils.js
+++ src/assets/commonUtils.js
@@ -1,19 +1,12 @@
-document.querySelectorAll('button').forEach(button => {//模块被import时自动执行
-    if(!button.closest('form'))
-        button.setAttribute('type','button');//button的tupe包括submit、reset和button
-});
+document.querySelectorAll('button').forEach(b => {if (!b.form) b.type = 'button';});//button的tupe包括submit、reset和button
 export const $ = id => document.getElementById(id);
 export function setLang(textMap) {//{"zh-CN":[],"en":[]}这种紧凑的格式维护起来不太灵活，但有利于指示AI翻译//参考RFC 5646
-    const supported=Object.keys(textMap);
-    let lang = supported[0];
-    for (const nL of navigator.languages) {
-        const found = supported.find(x => x.toLowerCase() === nL.toLowerCase());
-        if (found){
-            lang=found;
-            break;
-        }
-    }
-    document.documentElement.lang=lang;//缺少的话不会弹出翻译提示
+    const supported = Object.keys(textMap);
+    const lang = navigator.languages
+        .map(nL => supported.find(sL => sL.toLowerCase() === nL.toLowerCase()))
+        .find(Boolean)
+        || supported[0];
+    document.documentElement.lang = lang;//缺少的话不会弹出翻译提示
     return textMap[lang];
 }
 export function listenToggle(obj,key,defaultValue=true){
